9ede7bfdb7dce46724a899882f770e80161d6036
Brian Batronis <bbatronis@yahoo.com>
2025-09-19 08:45:21 -0700

    Added LED blink for feedback

--- code.py
+++ code.py
@@ -5,8 +5,20 @@
 """CircuitPython Essentials Servo continuous rotation servo example"""
 import time
 import board
+import neopixel
 import pwmio
 from adafruit_motor import servo
+
+led = neopixel.NeoPixel(board.NEOPIXEL, 1)  # Change this line
+count = .5
+
+colors = {
+    'red': (255, 0, 0),
+    'green': (0, 255, 0),
+    'blue': (0, 0, 255),
+    'purple': (128, 0, 128),
+    'off': (0, 0, 0)
+}
 
 # create a PWMOut object on Pin A2.
 pwm = pwmio.PWMOut(board.A2, frequency=50)
@@ -16,14 +28,18 @@ my_servo = servo.ContinuousServo(pwm)
 
 while True:
     print("forward")
+    led[0] = colors['green']
     my_servo.throttle = 1.0
-    time.sleep(2.0)
+    time.sleep(4.0)
     print("stop")
+    led[0] = colors['red']
     my_servo.throttle = 0.0
-    time.sleep(2.0)
+    time.sleep(4.0)
     print("reverse")
+    led[0] = colors['purple']
     my_servo.throttle = -1.0
-    time.sleep(2.0)
+    time.sleep(4.0)
     print("stop")
+    led[0] = colors['red']
     my_servo.throttle = 0.0
     time.sleep(4.0)
